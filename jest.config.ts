56397293a2c5b04d02c665a0bc3dbc53b1d31b3b
Kaue Campos <kauecampos01@hotmail.com>
2022-05-04 04:05:21 -0300

    Atualização transform para run code

--- jest.config.ts
+++ jest.config.ts
@@ -174,27 +174,8 @@ export default {
 
   // A map from regular expressions to paths to transformers
    transform: {
-     "^.+\\.(t|j)sx?$": [
-       "@swc/jest",
-       {
-         jsc:{
-           parser:{
-             syntax: 'typescript',
-             tsx: false,
-             decorators: true,
-           },
-           target: 'es2017',
-           keepClassNames: true,
-           transform:{
-             legacyDecorator: true,
-             decoratorMetadata: true,
-           },
-         },
-         module: 'es6',
-         noInterop: false,
-       },
-     ],
-   },
+      "^.+\\.(t|j)sx?$": ["@swc/jest"],
+    },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
